711b78c91cf2bdda024f15964d37059784e7b810
Sumit_Acharya <sumitacharya5948@gmail.com>
2026-03-23 18:15:26 +0530

    Delete Unit 1 Codes/set operations.py

--- Unit 1 Codes/set operations.py
+++
@@ -1,9 +0,0 @@
-# creating two sets
-# union, intersection, difference
-# add new element in set1
-# remove any element in set2
-set1 = {1, 2, 3, 4, 5}
-set2 = {6, 7, 8, 9, 10}
-print(set1.union(set1))
-print(set2.intersection(set2))
-
